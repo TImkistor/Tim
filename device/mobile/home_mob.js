14ede75ba8c5f3fd373bdf456da8915ec80c995d
Timkistor <tim.kisslove@gmail.com>
2024-11-29 19:39:45 +0300

    0.1.6 Mobile home

--- device/mobile/home_mob.js
+++ device/mobile/home_mob.js
@@ -2,7 +2,7 @@ import DIV from '../lib_element/div.js';
 import H1 from '../lib_element/h1.js'
 import IMG from '../lib_element/img.js';
 
-export default function home_pc(){
+export default function home_mob(){
         const div = new DIV('home_div','90%',window.innerHeight,'black')
         div.element.style.marginTop = "5%"
         div.element.style.marginLeft = "5%"
@@ -13,54 +13,65 @@ export default function home_pc(){
         div.appendTo(document.body)
 
         const h1 = new H1('home_h1','RusAnonymTeam pressent','white','center');
-        h1.element.style.fontSize = "50"
+        h1.element.style.fontSize = "60"
         h1.element.style.textShadow = "blue 1px 0 10px"
-        h1.element.style.paddingTop = "10%"
+        h1.element.style.paddingTop = "40%"
         h1.appendTo(div.element)
         h1.animationShiftText(50)
           .then(() => {
-            const h2 = new H1('home_h2', 'Занимаемся раработкой web приложений', 'white', 'center');
-            h2.element.style.fontSize = "50"
+            const h2 = new H1('home_h2', 'Занимаемся раработкой', 'white', 'center');
+            h2.element.style.fontSize = "60"
             h2.element.style.fontWeight = "normal"
             h2.element.style.textShadow = "blue 1px 0 10px"
             h2.appendTo(div.element)
             return h2.animationShiftText(50);
           })
           .then(() => {
-            const h3 = new H1('home_h3', 'Мои контакты', 'white', 'center');
-            h3.element.style.fontSize = "50"
+            const h3 = new H1('home_h3', 'Web приложений', 'white', 'center');
+            h3.element.style.fontSize = "60"
             h3.element.style.fontWeight = "normal"
+            h3.element.style.textShadow = "blue 1px 0 10px"
             h3.appendTo(div.element)
             return h3.animationShiftText(50);
+          }).then(()=>{
+            const h4 = new H1('home_h4', 'Мои контакты', 'white', 'center');
+            h4.element.style.fontSize = "60"
+            h4.element.style.fontWeight = "normal"
+            h4.element.style.textShadow = "blue 1px 0 10px"
+            h4.appendTo(div.element)
+            return h4.animationShiftText(50);
           })
           .then(()=>{
-            const div2 = new DIV('home_div2','50%','10%','black') 
+            const div2 = new DIV('home_div2','50%','20%','black') 
             div2.element.style.display = "flex"
             div2.element.style.marginLeft = "25%"
-            div2.element.style.justifyContent = "center"
+            div2.element.style.flexDirection = "column"
             div2.element.style.alignItems = "center"
             div2.appendTo(div.element)
 
-            const img1 = new IMG('home_img1','https://github.githubassets.com/assets/GitHub-Mark-ea2971cee799.png','13%','100%')
+            const img1 = new IMG('home_img1','https://www.rus-anonym.xyz/static/media/ea36da21aaf197498b4f.png','180px','180px')
             img1.element.style.borderRadius = '100%'
+            img1.element.style.position = "realitive"
             img1.animationAdvent(10)
             img1.AnimationRotate("mouseover","right")
             img1.AnimationRotate("mouseleave","left")
             img1.link("click","https://github.com/rus-https")
             img1.appendTo(div2.element)
 
-            const img2 = new IMG('home_img2','https://www.rus-anonym.xyz/static/media/76149e285a5aa89124f2.svg','13%','100%')
+            const img2 = new IMG('home_img2','https://www.rus-anonym.xyz/static/media/76149e285a5aa89124f2.svg','180px','180px')
             img2.element.style.borderRadius = '100%'
-            img2.element.style.marginLeft = "3%"
+            img2.element.style.position = "realitive"
+            img2.element.style.marginTop = "10%"
             img2.animationAdvent(10)
             img2.AnimationRotate("mouseover","right")
             img2.AnimationRotate("mouseleave","left")
             img2.link("click","https://t.me/rus-https")
             img2.appendTo(div2.element)
  
-            const img3 = new IMG('home_img3','https://www.rus-anonym.xyz/static/media/18e3dc57f0a7665e6ec7.svg','13%','100%')
+            const img3 = new IMG('home_img3','https://www.rus-anonym.xyz/static/media/18e3dc57f0a7665e6ec7.svg','180px','180px')
             img3.element.style.borderRadius = '100%'
-            img3.element.style.marginLeft = "3%"
+            img3.element.style.position = "realitive"
+            img3.element.style.marginTop = "10%"
             img3.animationAdvent(10)
             img3.AnimationRotate("mouseover","right")
             img3.AnimationRotate("mouseleave","left")
--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 import home_pc from "./device/pc/home_pc.js";
+import home_mob from "./device/mobile/home_mob.js";
 window.onload = function main() {
-    home_pc()
+  if (window.innerWidth > 1000){
+    home_pc(window.innerHeight)
+  }
+  else{
+    home_mob(window.innerHeight)
+  }
   }
