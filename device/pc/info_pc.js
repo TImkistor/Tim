f9c94d1e780d914537c49ab05c73b42168c1cbe5
Timkistor <tim.kisslove@gmail.com>
2024-12-01 19:41:50 +0300

    0.2.2 info_me step 1

--- device/pc/info_pc.js
+++ device/pc/info_pc.js
@@ -14,9 +14,70 @@ export default function info_pc(){
         div.animationShadow('mouseleave','0px 0px 20px 5px blue')
         div.appendTo(document.body)
 
-        const div2 = new DIV('info_div2',"100%","35%",'red')
+        const div2 = new DIV('info_div2',"100%","35%",)
+        div2.element.style.display = "flex"
+        div2.element.style.justifyContent = 'center'
+        div2.element.style.alignItems = "center"
         div2.appendTo(div.element)
-         
+
+        const div3 = new DIV('info_div2',"50%","100%",)
+        div3.element.style.display = "flex"
+        div3.element.style.justifyContent = 'center'
+        div3.element.style.alignItems = "center"
+        div3.appendTo(div2.element)
+
+        const img = new IMG('info_img1','https://sun9-77.userapi.com/impg/RwRExZHFyeETot29QAQ7RqhybUlkOLmcB6c1gg/BDqy1h0p1G0.jpg?size=640x640&quality=95&sign=7e357c39ab6fd68cce0dffa41b8b110d&type=album')
+        img.element.style.height = "90%"
+        img.appendTo(div3.element)
+        
+        const div4 = new DIV('info_div2',"50%","100%",)
+        div4.appendTo(div2.element)
+        
+        const p1 = new P()
+        p1.element.innerHTML = "<b>Имя:</b> Тимур"
+        p1.element.style.color = "white"
+        p1.appendTo(div4.element)
+
+        const p2 = new P()
+        p2.element.innerHTML = "<b>Город:</b> Москва"
+        p2.element.style.color = "white"
+        p2.appendTo(div4.element)
+
+        const p3 = new P()
+        p3.element.innerHTML = "<b>Специальность:</b> Техник по защите информации"
+        p3.element.style.color = "white"
+        p3.appendTo(div4.element)
+
+        const p4 = new P()
+        p4.element.innerHTML = "<b>Оброзавание:</b> Неоконченое высшее и среднее техническое"
+        p4.element.style.color = "white"
+        p4.appendTo(div4.element)
+
+        const p5 = new P()
+        p5.element.innerHTML = "<b>Вид работы:</b> Удаленая или работа в офисе не более 8 часов"
+        p5.element.style.color = "white"
+        p5.appendTo(div4.element)
+
+        const p6 = new P()
+        p6.element.innerHTML = "<b>Зарплата:</b> 500Р в час"
+        p6.element.style.color = "white"
+        p6.appendTo(div4.element)
+
+        const p7 = new P()
+        p7.element.innerHTML = "<b>Переезды:</b> Командировкам и переездам не готов"
+        p7.element.style.color = "white"
+        p7.appendTo(div4.element)
+
+        const p8 = new P()
+        p8.element.innerHTML = "<b>Гражанство:</b> РФ"
+        p8.element.style.color = "white"
+        p8.appendTo(div4.element)
+
+        const p9 = new P()
+        p9.element.innerHTML = "<b>Владениями языками:</b> Русский(разговорный) Англиский(на уровне чтения технической документации)"
+        p9.element.style.color = "white"
+        p9.appendTo(div4.element)
+
         const h1 = new H1 ('home_h1','Мой стек','white','center')
         h1.appendTo(div.element)
 
@@ -24,13 +85,13 @@ export default function info_pc(){
         h2.element.style.fontWeight = "normal"
         h2.appendTo(div.element)
 
-        const div3 = new DIV('info_div3',"100%","10%",'blue')
-        div3.element.style.display = "flex"
-        div3.element.style.justifyContent = 'center'
-        div3.element.style.alignItems = "top"
-        div3.appendTo(div.element)
+        const div5 = new DIV('info_div3',"100%","10%",'blue')
+        div5.element.style.display = "flex"
+        div5.element.style.justifyContent = 'center'
+        div5.element.style.alignItems = "top"
+        div5.appendTo(div.element)
 
-        const div4 = new DIV('info_div3',"100%","30%",'green')
-        div4.appendTo(div.element)
+        const div6 = new DIV('info_div3',"100%","30%",'green')
+        div6.appendTo(div.element)
 
 }
